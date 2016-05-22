17122893e526371b7d6254eaed3c95ac4f0f9aee
Hector Wallen <hwallen@atex.com>
2016-05-22 22:29:47 +0200

    updated powersort to 1.0.1

--- powersort.rb
+++ powersort.rb
@@ -1,9 +1,9 @@
 class Powersort < Formula
   desc "a text sorter that sorts by column and/or regex"
   homepage ""
-  url "https://github.com/Gherkin/powersort/releases/download/v1.0/1.0.tar.gz"
-  version "1.0"
-  sha256 "7c6101e4722b1acec6d65596689b2d669f6669e51521f4773588b2ac7e96574a"
+  url "https://github.com/Gherkin/powersort/releases/download/v1.0.1/1.0.1.tar.gz"
+  version "1.0.1"
+  sha256 "68547f4200d858365576358dd8feb8b3e84eb38a00b21270077699db1a75bf08"
 
   def install
 	bin.install 'powersort'
