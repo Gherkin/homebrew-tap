934de533ec18e3708f6c4936eb0cf5a975129dc5
Hector Wallen <hwallen@atex.com>
2017-09-13 11:58:45 +0200

    added the wrong url to pexecute

--- pexecute.rb
+++ pexecute.rb
@@ -1,7 +1,7 @@
 class Pexecute < Formula
   desc ""
   homepage ""
-  url "https://github.com/Gherkin/pidport/releases/download/v1.0.0/1.0.0.tar.gz"
+  url "https://github.com/Gherkin/pexecute/archive/1.0.tar.gz"
   version "1.0"
   sha256 "a6e45f46c40c97ea16f0cdd31709d7381cab7793e2364bbd295bf954618847e7"
 
