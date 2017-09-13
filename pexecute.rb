995982b79aa211289b964b844fabdd22dc1df9fb
Hector Wallen <hwallen@atex.com>
2017-09-13 12:02:56 +0200

    updated the sha on pexecute

--- pexecute.rb
+++ pexecute.rb
@@ -3,7 +3,7 @@ class Pexecute < Formula
   homepage ""
   url "https://github.com/Gherkin/pexecute/archive/1.0.tar.gz"
   version "1.0"
-  sha256 "a6e45f46c40c97ea16f0cdd31709d7381cab7793e2364bbd295bf954618847e7"
+  sha256 "4e5a9f978fc144531b10083e93c3d490e9d6a4c01b90f63396f840fe4420c93d"
 
   depends_on "coreutils"
 
